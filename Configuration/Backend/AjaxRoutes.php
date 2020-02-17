481a65db44dc7ab3eb05df2618ca79e1f2496e9a
koehlersimon <simonkohler@192.168.2.1>
2020-02-17 18:01:37 -0500

    Route for ajax call

--- Configuration/Backend/AjaxRoutes.php
+++ Configuration/Backend/AjaxRoutes.php
@@ -7,6 +7,10 @@ use SIMONKOEHLER\Slug\Controller;
  */
 
 return [
+    'ajaxList' => [
+        'path' => '/slug/ajaxList',
+        'target' => Controller\AjaxController::class . '::ajaxList'
+    ],
     'savePageSlug' => [
         'path' => '/slug/savePageSlug',
         'target' => Controller\AjaxController::class . '::savePageSlug'
