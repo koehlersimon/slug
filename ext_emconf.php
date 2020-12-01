7a270c5b3f825c10e417610c0c51cf8b2e0929e8
Jens <neumann.jens@freenet.de>
2020-12-01 17:01:08 +0100

    [BUGFIX] fix version constraint in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '3.0.3',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-10.2.99',
+            'typo3' => '10.2.0-10.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
